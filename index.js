7d155f58c72c8833cdccae9e1d7ce3f0c40f8b6c
HCS Learning <learning@hardcybersoft.cl>
2021-05-27 17:55:28 -0400

    execute index.js from npm start and console.dir de result insertOne() method

--- index.js
+++ index.js
@@ -2,12 +2,13 @@ const { MongoClient } = require('mongodb');
 
 // TODO: valores por ENV
 require('dotenv').config();
-const user = process.env.DB_USER;
-const password = process.env.DB_PASS;
-const dbname = process.env.DB_NAME;
-const host = process.env.DB_HOST;
-const options = process.env.DB_OPT;
-const uri = `mongodb+srv://${user}:${password}@${host}/${dbname}?${options}`;
+const user      = process.env.DB_USER;
+const password  = process.env.DB_PASS;
+const dbname    = process.env.DB_NAME;
+const protocol  = process.env.DB_PROTOCOL
+const host      = process.env.DB_HOST;
+const options   = process.env.DB_OPT;
+const uri       = `${protocol}://${user}:${password}@${host}/${dbname}?${options}`;
 
 const client = new MongoClient(uri);
 
@@ -16,8 +17,11 @@ async function run() {
         await client.connect();
         const db = client.db( dbname );
         const animalesCollection = db.collection('animales');
-        await animalesCollection.insert({nombre: "Chola"});
+        const result = await animalesCollection.insertOne({nombre: "Farkas"});
         const animales = await animalesCollection.find({});
+        console.log("insertOne() result:")
+        console.dir(result)
+        console.log("find({}) result:")
         animales.forEach( console.dir )
     } finally {
         await client.close();
